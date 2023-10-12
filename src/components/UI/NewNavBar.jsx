a6ba7cf99b0540c7882f77a1342cd5f9da735a00
KLUNNYYY <klunnyyy@gmail.com>
2023-10-12 11:42:23 +0300

    draft project

--- src/components/UI/NewNavBar.jsx
+++ src/components/UI/NewNavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NewNavBar() {
   return (
     <nav class="navMenu">
-    <a href="#">Home</a>
+    <a href="/">Home</a>
     <a href="#">Blog</a>
     <a href="#">Work</a>
     <a href="#">About</a>
