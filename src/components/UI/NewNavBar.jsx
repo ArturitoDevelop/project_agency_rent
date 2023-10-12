8aebc3c4522e166e0de06fd2d1a5a79ecb15c7df
KLUNNYYY <klunnyyy@gmail.com>
2023-10-12 11:47:28 +0300

    correct ways in the NavBar

--- src/components/UI/NewNavBar.jsx
+++ src/components/UI/NewNavBar.jsx
@@ -4,8 +4,8 @@ export default function NewNavBar() {
   return (
     <nav class="navMenu">
     <a href="/">Home</a>
-    <a href="#">Blog</a>
-    <a href="#">Work</a>
+    <a href="/authPage">SignUp</a>
+    <a href="/loginPage">Login</a>
     <a href="#">About</a>
     <div class="dot"></div>
   </nav>
