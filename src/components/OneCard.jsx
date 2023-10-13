d2cdd6d0ed3fbbb50954225400c876fb45cc8212
KLUNNYYY <klunnyyy@gmail.com>
2023-10-13 13:06:23 +0300

    correct working of Favs

--- src/components/OneCard.jsx
+++ src/components/OneCard.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import {  Card, Carousel } from 'react-bootstrap';
+import { Card, Carousel } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import DeleteBtn from './UI/DeleteBtn';
 
-
-export default function OneCard({ post, handlerOnDelete, user, favoriteHandler, deleteFavHandler, currentUser }) {
-
+export default function OneCard({
+  post,
+  handlerOnDelete,
+  user,
+  favoriteHandler,
+  deleteFavHandler,
+  currentUser,
+}) {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex) => {
@@ -46,13 +51,11 @@ export default function OneCard({ post, handlerOnDelete, user, favoriteHandler, 
               </button>
             </>
           )}
-          {currentUser && (<>
-            <button className="btn" type="button" onClick={() => favoriteHandler(post.id)}>
-              <img className="iconfav" src="/img/free-icon-home-1098486.png" alt="" />
-            </button>
-             <DeleteBtn deleteFavHandler={deleteFavHandler} postId={post.id}  />
-          </>
-          )}
+
+          <button className="btn" type="button" onClick={() => favoriteHandler(post.id)}>
+            <img className="iconfav" src="/img/free-icon-home-1098486.png" alt="" />
+          </button>
+          <DeleteBtn deleteFavHandler={deleteFavHandler} postId={post.id} />
         </div>
       </Card.Body>
     </Card>
